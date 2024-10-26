23a0097108549494208713c00b9f75ed25e3c427
sauna1137 <soneko2102@gmail.com>
2024-10-26 22:27:42 +0900

    Initial Commit

---
+++ tca_practice/tca_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_practiceApp.swift
+//  tca_practice
+//
+//  Created by KS on 2024/10/26.
+//
+
+import SwiftUI
+
+@main
+struct tca_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
