f7620c55cda49556633c19c6592f029e63d1600c
sauna1137 <soneko2102@gmail.com>
2024-11-19 09:41:12 +0900

    Add BindingsForm.swift

---
+++ tca_practice/BindingsForm.swift
@@ -0,0 +1,18 @@
+//
+//  BindingsForm.swift
+//  tca_practice
+//
+//  Created by KS on 2024/11/14.
+//
+
+import SwiftUI
+
+struct BindingsForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BindingsForm()
+}
